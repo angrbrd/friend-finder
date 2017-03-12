f53a9d19fcbc46c6e8227e7416ad9c489a465709
Anna D Derbakova <hiopa16@gmail.com>
2017-03-12 16:54:46 -0400

    Adding basic Express server functionality

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,7 @@
+// Pull in required dependencies
+var path = require("path");
+
+module.exports = function(app) {
+	console.log('___ENTER htmlRoutes.js___');
+
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+// Pull in required dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Configure the Express application
+var app = express();
+var PORT = 3000;
+
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log("Friend Finder app is listening on PORT: " + PORT);
+});
