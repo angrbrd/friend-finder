fe186190ceb2e5230ea6829eaecb5fd88c0b780d
Anna D Derbakova <hiopa16@gmail.com>
2017-03-12 17:44:35 -0400

    Adding test data to friends list

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+console.log('___ENTER friends.js___');
+
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: "Ahmed",
+		photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"4",
+			"5",
+			"1",
+			"2",
+			"5",
+			"4",
+			"1"
+		]
+	},
+	{
+		name: "Jacob Deming",
+		photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+		scores: [
+			"4",
+			"2",
+			"5",
+			"1",
+			"3",
+			"2",
+			"2",
+			"1",
+			"3",
+			"2"
+		]
+	},
+	{
+		name: "Jeremiah Scanlon",
+		photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+		scores: [
+			"5",
+			"2",
+			"2",
+			"2",
+			"4",
+			"1",
+			"3",
+			"2",
+			"5",
+			"5"
+		]
+	}
+];
+
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,15 @@
 // Pull in required dependencies
 var path = require("path");
 
+// Import the list of friend entries
+var friends = require('../data/friends.js');
+
 // Export API routes
 module.exports = function(app) {
 	console.log('___ENTER apiRoutes.js___');
 
+	// Total list of friend entries
+	app.get("/api/friends", function(req, res) {
+		res.json(friends);
+	});
 };
