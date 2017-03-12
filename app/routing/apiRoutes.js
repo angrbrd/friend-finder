31de3edc09a0ab2fc347e3d441426417024fc8c1
Anna D Derbakova <hiopa16@gmail.com>
2017-03-12 19:19:49 -0400

    Adding POST route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,5 @@
 // Pull in required dependencies
-var path = require("path");
+var path = require('path');
 
 // Import the list of friend entries
 var friends = require('../data/friends.js');
@@ -9,7 +9,20 @@ module.exports = function(app) {
 	console.log('___ENTER apiRoutes.js___');
 
 	// Total list of friend entries
-	app.get("/api/friends", function(req, res) {
+	app.get('/api/friends', function(req, res) {
 		res.json(friends);
+	});
+
+	// Add new friend entry
+	app.post('/api/friends', function(req, res) {
+		// Capture the user input object
+		var userInput = req.body;
+		console.log('userInput = ' + JSON.stringify(userInput));
+
+		// Add new user
+		friends.push(userInput);
+
+		// Send appropriate response
+		res.json({status: 'OK'});
 	});
 };
