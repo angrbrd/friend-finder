8e2ffdc3758f783213e0a1097ab136f5fe64ce70
Anna D Derbakova <hiopa16@gmail.com>
2017-03-12 23:33:41 -0400

    Removing debug statements

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,10 +17,10 @@ module.exports = function(app) {
 	app.post('/api/friends', function(req, res) {
 		// Capture the user input object
 		var userInput = req.body;
-		console.log('userInput = ' + JSON.stringify(userInput));
+		// console.log('userInput = ' + JSON.stringify(userInput));
 
 		var userResponses = userInput.scores;
-		console.log('userResponses = ' + userResponses);
+		// console.log('userResponses = ' + userResponses);
 
 		// Compute best friend match
 		var matchName = '';
@@ -29,20 +29,20 @@ module.exports = function(app) {
 
 		// Examine all existing friends in the list
 		for (var i = 0; i < friends.length; i++) {
-			console.log('friend = ' + JSON.stringify(friends[i]));
+			// console.log('friend = ' + JSON.stringify(friends[i]));
 
 			// Compute differenes for each question
 			var diff = 0;
 			for (var j = 0; j < userResponses.length; j++) {
 				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
 			}
-			console.log('diff = ' + diff);
+			// console.log('diff = ' + diff);
 
 			// If lowest difference, record the friend match
 			if (diff < totalDifference) {
-				console.log('Closest match found = ' + diff);
-				console.log('Friend name = ' + friends[i].name);
-				console.log('Friend image = ' + friends[i].photo);
+				// console.log('Closest match found = ' + diff);
+				// console.log('Friend name = ' + friends[i].name);
+				// console.log('Friend image = ' + friends[i].photo);
 
 				totalDifference = diff;
 				matchName = friends[i].name;
