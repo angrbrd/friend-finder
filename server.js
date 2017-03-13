4fa658dceefebbdba1a9ec11b8adde848a45b2d4
Anna D Derbakova <hiopa16@gmail.com>
2017-03-13 00:04:14 -0400

    Heroku deployment changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 // Configure the Express application
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT;
 
 // Expose the public directory to access CSS files
 app.use(express.static(path.join(__dirname, './app/public')));
